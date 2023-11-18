f0feadb8e2dbd337da5fba27da31870bca8496dd
dmichaels411 <dmichaels411@gmail.com>
2023-11-17 20:26:49 -0500

    added ability to modify the shopping cart

--- ECommerceWebFormsDemo/Logic/ShoppingCartActions.cs
+++ ECommerceWebFormsDemo/Logic/ShoppingCartActions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 
 namespace ECommerceWebFormsDemo.Logic
@@ -75,6 +76,132 @@ namespace ECommerceWebFormsDemo.Logic
             ShoppingCartId = GetCartId();
 
             return _db.CartItems.Where(c => c.CartId == ShoppingCartId).ToList();
+        }
+
+        public decimal GetTotal()
+        {
+            ShoppingCartId = GetCartId();
+
+            decimal? total = decimal.Zero;
+            total = (decimal?)(from cartItems in _db.CartItems
+                               where cartItems.CartId == ShoppingCartId
+                               select (int?)cartItems.Quantity *
+                               cartItems.Product.UnitPrice).Sum();
+            return total ?? decimal.Zero;
+        }
+
+        public ShoppingCartActions GetCart(HttpContext context)
+        {
+            using (var cart = new ShoppingCartActions())
+            {
+                cart.ShoppingCartId = cart.GetCartId();
+                return cart;
+            }
+        }
+
+        public void UpdateShoppingCartDatabase(String cartId, ShoppingCartUpdates[] cartItemUpdates)
+        {
+            using (var db = new ECommerceWebFormsDemo.Models.ProductContext())
+            {
+                try
+                {
+                    int cartItemCount = cartItemUpdates.Count();
+                    List<CartItem> myCart = GetCartItems();
+                    foreach (var cartItem in myCart)
+                    {
+                        // Iterate through all rows within shopping cart list
+                        for (int i = 0; i < cartItemCount; i++)
+                        {
+                            if (cartItem.Product.ProductID == cartItemUpdates[i].ProductId)
+                            {
+                                if (cartItemUpdates[i].PurchaseQuantity < 1 || cartItemUpdates[i].RemoveItem == true)
+                                {
+                                    RemoveItem(cartId, cartItem.ProductId);
+                                }
+                                else
+                                {
+                                    UpdateItem(cartId, cartItem.ProductId, cartItemUpdates[i].PurchaseQuantity);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("ERROR : Unable to Update Cart Database - " + ex.Message.ToString(), ex);
+                }
+            }
+        }
+
+        public void RemoveItem(string removeCartID, int removeProductID)
+        {
+            using (var _db = new ECommerceWebFormsDemo.Models.ProductContext())
+            {
+                try
+                {
+                    var myItem = (from c in _db.CartItems where c.CartId == removeCartID && c.Product.ProductID == removeProductID select c).FirstOrDefault();
+                    if (myItem != null)
+                    {
+                        _db.CartItems.Remove(myItem);
+                        _db.SaveChanges();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("ERROR : Unable to Remove Cart Item - " + ex.Message.ToString(), ex);
+                }
+            }
+        }
+
+        public void UpdateItem(string updateCartID, int updateProductID, int quantity)
+        {
+            using (var _db = new ProductContext())
+            {
+                try
+                {
+                    var myItem = (from c in _db.CartItems where c.CartId == updateCartID && c.Product.ProductID == updateProductID select c).FirstOrDefault();
+                    if (myItem != null)
+                    {
+                        myItem.Quantity = quantity;
+                        _db.SaveChanges();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("ERROR : Unable to Update Cart Item - " + ex.Message.ToString(), ex);
+                }
+            }
+        }
+
+        public void EmptyCart()
+        {
+            ShoppingCartId = GetCartId();
+            var cartItems = _db.CartItems.Where(
+                c => c.CartId == ShoppingCartId);
+            foreach (var cartItem in cartItems)
+            {
+                _db.CartItems.Remove(cartItem);
+            }
+            _db.SaveChanges();
+        }
+
+        public int GetCount()
+        {
+            ShoppingCartId = GetCartId();
+
+            // Get the count of each item in the cart and sum them up
+            int? count = (from cartItems in _db.CartItems
+                          where cartItems.CartId == ShoppingCartId
+                          select (int?)cartItems.Quantity).Sum();
+            // Return 0 if all entries are null
+            return count ?? 0;
+        }
+
+        public struct ShoppingCartUpdates
+        {
+            public int ProductId;
+            public int PurchaseQuantity;
+            public bool RemoveItem;
         }
     }
 }
